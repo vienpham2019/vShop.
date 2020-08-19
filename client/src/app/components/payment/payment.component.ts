b8f1917468e3fa8573c8992ae7e55d38d24beff0
vienpham2019 <vienpham2019@gmail.com>
2020-08-19 18:05:19 -0500

    add remove item aleart to payment component

--- client/src/app/components/payment/payment.component.ts
+++ client/src/app/components/payment/payment.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder , Validators , FormGroup } from '@angular/forms'
+import { Router } from '@angular/router'
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-payment',
@@ -8,7 +10,7 @@ import { FormBuilder , Validators , FormGroup } from '@angular/forms'
 })
 export class PaymentComponent implements OnInit {
 
-  constructor(private _fb: FormBuilder) { }
+  constructor(private _fb: FormBuilder , private router: Router) { }
 
   shipping_details: any[] = [
     {type: "Free Shipping" , detail: "Delivery in 8 - 10 working days" , price: 0},
@@ -18,7 +20,7 @@ export class PaymentComponent implements OnInit {
   ]
 
   shopping_items: any[] = [
-    {title: "Cotton floral print Dress" , price: 40.00 , size: "M" , color: "Red" , amount: 1 , img: "https://www.shopjessicabuurman.com/image/cache/catalog/2020/clothing/47325/1-buy-women-sakix-asymmetric-hem-long-sleeves-top-black-800x800.png"},
+    {title: "Cotton floral print Dress" , price: 40.00 , size: "M" , color: "Red" , amount: 1 , img: "https://assets.adidas.com/images/h_320,f_auto,q_auto:sensitive,fl_lossy/449c838942da409f8ba9a97f00d3cffe_9366/Runfalcon_Shoes_Black_F36199_01_standard.jpg"},
     {title: "Cotton floral print Dress" , price: 40.00 , size: "M" , color: "Red" , amount: 1 , img: "https://www.shopjessicabuurman.com/image/cache/catalog/2020/clothing/47325/1-buy-women-sakix-asymmetric-hem-long-sleeves-top-black-800x800.png"},
     {title: "Cotton floral print Dress" , price: 40.00 , size: "M" , color: "Red" , amount: 2 , img: "https://www.shopjessicabuurman.com/image/cache/catalog/2020/clothing/47325/1-buy-women-sakix-asymmetric-hem-long-sleeves-top-black-800x800.png"},
     {title: "Cotton floral print Dress" , price: 40.00 , size: "M" , color: "Red" , amount: 3 , img: "https://www.shopjessicabuurman.com/image/cache/catalog/2020/clothing/47325/1-buy-women-sakix-asymmetric-hem-long-sleeves-top-black-800x800.png"},
@@ -31,7 +33,7 @@ export class PaymentComponent implements OnInit {
     {title: "Cotton floral print Dress" , price: 40.00 , size: "M" , color: "Red" , amount: 1 , img: "https://www.shopjessicabuurman.com/image/cache/catalog/2020/clothing/47325/1-buy-women-sakix-asymmetric-hem-long-sleeves-top-black-800x800.png"},
     {title: "Cotton floral print Dress" , price: 40.00 , size: "M" , color: "Red" , amount: 1 , img: "https://www.shopjessicabuurman.com/image/cache/catalog/2020/clothing/47325/1-buy-women-sakix-asymmetric-hem-long-sleeves-top-black-800x800.png"},
     {title: "Cotton floral print Dress" , price: 40.00 , size: "M" , color: "Red" , amount: 1 , img: "https://www.shopjessicabuurman.com/image/cache/catalog/2020/clothing/47325/1-buy-women-sakix-asymmetric-hem-long-sleeves-top-black-800x800.png"},
-    {title: "Cotton floral print Dress" , price: 40.00 , size: "M" , color: "Red" , amount: 1 , img: "https://www.shopjessicabuurman.com/image/cache/catalog/2020/clothing/47325/1-buy-women-sakix-asymmetric-hem-long-sleeves-top-black-800x800.png"},
+    {title: "Cotton floral print Dress" , price: 40.00 , size: "" , color: "Red" , amount: 1 , img: "https://www.shopjessicabuurman.com/image/cache/catalog/2020/clothing/47325/1-buy-women-sakix-asymmetric-hem-long-sleeves-top-black-800x800.png"},
   ]
 
   payment_form: FormGroup
@@ -97,6 +99,42 @@ export class PaymentComponent implements OnInit {
     this.subtotal = this.shopping_items.reduce( (sum,item) => sum + (item.price * item.amount) , 0)
     this.tax = this.subtotal * (8.25 / 100)
     this.total = this.subtotal + this.tax + this.payment_form.value.shipping_details.price
+  }
+
+  checkout() {
+    this.submit_invalid = this.payment_form.status === "INVALID"
+    if(!this.submit_invalid){
+      this.router.navigate(['/'])
+    }
+  }
+
+  removeItem(item , index) {
+    Swal.fire({
+      html: 
+      `
+        <div class="card mb-2">
+          <img src=${item.img} class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">${item.title}</h5>
+          </div>
+        </div>
+        <h4> Are you sure to remove this items? </h4> 
+      `, 
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, remove it!',
+    }).then((result) => {
+      if (result.value) {
+        this.shopping_items.splice(index , 1)
+        this.getTotal()
+        Swal.fire(
+          'Remove!',
+          `' ${item.title} ' has been remove form your shopping cart.`,
+          'success'
+        )
+      }
+    })
   }
 
 }
