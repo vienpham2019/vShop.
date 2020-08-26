34e3e2371aa4a10bf118f549c68f32c918f6d249
vienpham2019 <vienpham2019@gmail.com>
2020-08-26 18:51:37 -0500

    add login form

--- client/src/app/reducers/user.reducer.ts
+++ client/src/app/reducers/user.reducer.ts
@@ -7,10 +7,6 @@ export const initState: User = {
     first_name: '',
     last_name: '',
     shipping_details: [
-        {first_name: 'Daniel' , last_name: 'Robinson', email: 'vienpham2019@gmail.com' , address1: '3997 Raccoon Run', address2: '',city: 'New', state: 'Kingston', zip: '45644', country: 'United States', phone: '6146389574' },
-        {first_name: 'Daniel' , last_name: 'Robinson', email: 'vienpham2019@gmail.com' , address1: '3997 Raccoon Run', address2: '',city: 'New', state: 'Kingston', zip: '45644', country: 'United States', phone: '6146389574' },
-        {first_name: 'Daniel' , last_name: 'Robinson', email: 'vienpham2019@gmail.com' , address1: '3997 Raccoon Run', address2: '',city: 'New', state: 'Kingston', zip: '45644', country: 'United States', phone: '6146389574' },
-        {first_name: 'Daniel' , last_name: 'Robinson', email: 'vienpham2019@gmail.com' , address1: '3997 Raccoon Run', address2: '',city: 'New', state: 'Kingston', zip: '45644', country: 'United States', phone: '6146389574' }
     ],
     edit_shipping_index: null ,
     edit_shipping_detail: null ,
