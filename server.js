72a4546c733a196c31f55fca8583210eef23a66c
vienpham2019 <vienpham2019@gmail.com>
2020-08-27 17:52:06 -0500

    add server

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
+require('dotenv/config')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 app.use(express.urlencoded({extended: false}))
+
+// data base 
+mongoose.connect(process.env.MONGOOSE_URI , {useNewUrlParser: true , useCreateIndex: true , useUnifiedTopology: true})
+
+mongoose.connection.once('open' , () => console.log('Connect to database ....'))
 
 
 const PORT = process.env.PORT || 5000
